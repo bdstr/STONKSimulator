f15108eb0573f12ab688d5cbd6d72c4dcef99787
bdstr <marcin@kloczkowski.net>
2021-02-03 23:46:11 +0100

    Initial commit - basic project structure with database and Docker

---
+++ STONKSimulator-API/src/main/java/net/kloczkowski/STONKSimulator/API/StonkSimulatorApiApplication.java
@@ -0,0 +1,13 @@
+package net.kloczkowski.STONKSimulator.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonkSimulatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StonkSimulatorApiApplication.class, args);
+    }
+
+}
